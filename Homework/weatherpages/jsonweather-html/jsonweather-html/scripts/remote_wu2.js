e5d23ded8ce72dcd175b5556d85b9b8df7127d96
croftsr <cro11007@byui.edu>
2017-03-17 20:03:10 -0600

    Jquery bullcrap

---
+++ Homework/weatherpages/jsonweather-html/jsonweather-html/scripts/remote_wu2.js
@@ -0,0 +1,41 @@
+//Remote-wu2franklin
+// Current Location Scripts
+$(function() {
+
+
+                // Call the getData function, send the lat and long
+    getData(42.011902, -111.803757);
+
+           
+   
+});
+
+
+// Get the data from the wunderground API
+function getData(lat, long){
+$.ajax({ 
+url: "https://api.wunderground.com/api/a8b9ab8e9f5580f1/geolookup/conditions/astronomy/q/" + lat + "," + long + ".json",
+dataType: "jsonp",
+success: function(data) {
+console.log("success",data);
+
+
+$('#cityDisplay').prepend(data.location.city +', '+ data.location.state + ' ')
+$('#currentTemp').text(Math.round(data.current_observation.temp_f) +'\xB0 '+ 'F')
+$('#add1').append('With a visibility of '+data.current_observation.visibility_mi +' Miles.')
+$('#add2').append('Wind: '+ data.current_observation.wind_string)
+$('#add3').append('Relative Humidity: '+data.current_observation.relative_humidity)
+
+console.log("success", data);
+
+$("#cover").fadeOut(250);
+} 
+});
+
+}
+
+// A function for changing a string to TitleCase
+function toTitleCase(str){
+return str.replace(/\w+/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+}
+
