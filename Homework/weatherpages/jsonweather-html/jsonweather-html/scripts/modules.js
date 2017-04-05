7739b3c10bb772224eea8506424d5a7990bc93d0
croftsr <cro11007@byui.edu>
2017-04-05 17:24:08 -0600

    change of file for modules

--- Homework/weatherpages/jsonweather-html/jsonweather-html/scripts/modules.js
+++ Homework/weatherpages/jsonweather-html/jsonweather-html/scripts/modules.js
@@ -2,7 +2,7 @@
 // Pathes may need altered depending on site structure
 $(function () {
     $("#page-header").load("/finalproject/header.html");
-    console.log("test")
+   
    
     $("#page-footer").load("/finalproject/footer.html");
 
--- finalproject/modules.js
+++ finalproject/modules.js
@@ -2,9 +2,9 @@
 // Pathes may need altered depending on site structure
 $(function() {
     
-    $("#page-header").load("header.html");
-    console.log("test")
-    $("#page-footer").load("footer.html");
+    $("#page-header").load("/finalproject/header.html");
+   
+    $("#page-footer").load("/finalproject/footer.html");
 
 });
 
